b5363f939e547174e0bfc6ba273a160de70977c6
iab131 <enhe2006@gmail.com>
2024-10-14 21:21:29 -0400

    commit

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from './ui/Card'
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36 bg-black-100'>
+        <Card />
+    </div>
+    
+  )
+}
+
+export default Hero
