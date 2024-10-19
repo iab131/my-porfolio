7efa65bed00c37db426c04238001533a1a2d8a55
iab131 <enhe2006@gmail.com>
2024-10-19 01:13:34 -0400

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/Hero";
+import { Box } from "@/components/ui/Box";
 import { Card } from "@/components/ui/Card";
 // import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 h-screen">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-auto mx-auto sm:px-10 px-5 h-screen">
+      <div>
+        <Box/>
+      </div>
       <div className="max-w-7xl w-full">
         <Hero />
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Box } from './ui/Box'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import { Card } from './ui/Card'
+
 const Hero = () => {
   return (
-    <><div >
-      <Box />
-    </div>
-    <div>
+    <div className='h-fit w-full flex flex-col items-center pt-[50%] gap-10 '> 
+      <div>
         <TextGenerateEffect
           className="text-center text-[40px] md:text-5xl lg:text-6xl"
           words="Transforming Concepts into Seamless Experiences" />
-    </div></>
-    
-  )
+      </div>
+      <div>
+        <Card />
+      </div>   
+    </div>   
+  );
 }
 
 export default Hero
